c4e6fe6d0812e3dff72448f8100ea1c2cf446ab0
CDVC <cesarcamposeco415@gmail.com>
2025-08-15 10:38:22 -0600

    Clase menus

--- SMARTSTOCK.py
+++ SMARTSTOCK.py
@@ -1,13 +1,14 @@
-def Menu():
-    print("Bienvenido al inventario BingBong")
-    print("1.Registrar producto")
-    print("2.Mostrar productos registrados")
-    print("3.Bucar productos registrados")
-    print("4.Administración de productos")
-    print("5.Salir")
-def Q_S_Menu():
-    print("Mostrar productos ordenados(presione X para salir)")
-    print("de que forma desea ordenar los productos? ")
-    print("1.Por nombre")
-    print("2.Por stock")
-    print("3.Por precio")
+class MENU:
+    def Menu(self):
+        print("Bienvenido al inventario BingBong")
+        print("1.Registrar producto")
+        print("2.Mostrar productos registrados")
+        print("3.Bucar productos registrados")
+        print("4.Administración de productos")
+        print("5.Salir")
+    def Q_S_Menu(self):
+        print("Mostrar productos ordenados(presione X para salir)")
+        print("de que forma desea ordenar los productos? ")
+        print("1.Por nombre")
+        print("2.Por stock")
+        print("3.Por precio")
