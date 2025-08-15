596b69917a7bfe41b4c8badd9e2afd0338405522
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-15 16:56:41 -0600

    Clases y Menu

--- SMARTSTOCK.py
+++ SMARTSTOCK.py
@@ -12,4 +12,40 @@ class MENU:
         print("1.Por nombre")
         print("2.Por stock")
         print("3.Por precio")
+class Producto:
+    def __init__(self, codigo,nombre,categoria, stock, precio):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.stock = stock
+        self.precio = precio
 menu = MENU()
+allow = False
+try:
+    while allow == False:
+        menu.Menu()
+        opt = input("Ingrese la opción que desee: ")
+        match opt:
+            case "1":
+                cont = 0
+                num = int(input("Cuantos productos desea ingresar? "))
+                if num < 1 or num > 10:
+                    print("La cantidad ingresada no es valida")
+                else:
+                    for i in range(num):
+                        prin("")
+                       print(f"Ingreso del producto: {i}")
+
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                pass
+            case "5":
+                pass
+            case _:
+                print("La opción seleccionada no es valida")
+except ValueError:
+    print("Tipo de dato incorrecto")
+    #El try lo puse nomás para que se guíe jaja no está al 100
